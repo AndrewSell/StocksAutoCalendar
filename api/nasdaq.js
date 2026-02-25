6d12d731904dc5acb740d1fbc30ac1afa50885f9
AndrewSell <linjayden256@gmail.com>
2026-02-26 01:15:50 +0800

    Update nasdaq.js

--- api/nasdaq.js
+++ api/nasdaq.js
@@ -62,7 +62,7 @@ async function fetchEarningsData(date) {
             'accept-language': 'en-US,en;q=0.9',
             'origin': 'https://www.nasdaq.com',
             'referer': 'https://www.nasdaq.com/',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56'
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36'
         },
         method: 'GET',
         mode: 'cors'
